dc89e3232e3c84202fc08c03badba419f20e0034
Puppy_x <haoranxu1996@163.com>
2020-05-18 12:54:14 +0800

    test vcs

--- CapacityToShipPackagesWithin_D_Days_1011/src/Solution.java
+++ CapacityToShipPackagesWithin_D_Days_1011/src/Solution.java
@@ -2,8 +2,7 @@ import com.sun.scenario.animation.shared.ClipEnvelope;
 
 /**
  * @program: LeetCode
- * @description: A conveyor belt has packages that must be shipped from one port to another within D days.  来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/capacity-to-ship-packages-within-d-days
- * @author: 你的名字
+ * @description: A conveyor belt has packages that must be shipped from one port to another within D days.
  * @create: 2020-05-17 21:52
  **/
 public class Solution {
@@ -59,5 +58,6 @@ public class Solution {
             return 2;
         }
         else return 0;
+        //test vcs
     }
 }
